41cf59726ff1b19aaf6fd822fe594525e178cdb6
rfqv <rifqivirgiansyah@gmail.com>
2022-12-12 10:42:46 +0700

    part1

---
+++ app/src/main/java/mrifqiv/if31b/projectbesarpab1/SplashScreen1.java
@@ -0,0 +1,14 @@
+package mrifqiv.if31b.projectbesarpab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
